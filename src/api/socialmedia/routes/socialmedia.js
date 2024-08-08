431fc2ca3fee70b371ee2c04dbbcebaeb0da83f1
Nicholas Mason-Apps <nicholas.mason-apps@live.co.uk>
2024-08-08 10:47:04 +0100

    First commit

---
+++ src/api/socialmedia/routes/socialmedia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * socialmedia router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::socialmedia.socialmedia');
---
+++ src/api/socialmedia/services/socialmedia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * socialmedia service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::socialmedia.socialmedia');
