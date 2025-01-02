22444d256c87f0f18f5e184d884e1ecfd2c00c74
NicholasMason-Apps <95938071+NicholasMason-Apps@users.noreply.github.com>
2025-01-02 11:05:10 +0000

    Attempting again

--- src/api/contact/controllers/contact.js
+++ src/api/contact/controllers/contact.js
@@ -11,7 +11,7 @@ module.exports = {
             null,
                 {
                     params: {
-                        secret: process.env.RECAPTCHA_SECRET_KEY_PROD,
+                        secret: process.env.RECAPTCHA_SECRET_KEY_DEVELOP,
                         response: recaptcha
                     }
                 }
