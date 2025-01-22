e1e8e8fdbffa77fd05f7a4b19299c91d12b408b0
warwickunirealale <warwickunirealale@gmail.com>
2025-01-22 11:10:12 +0000

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,28 +1,15 @@
-module.exports = ({ env }) => ({
-    email: {
-        config: {
-            provider: 'nodemailer',
-            providerOptions: {
-                host: env('SMTP_HOST'), // The SMTP server (e.g., Gmail)
-                port: env.int('SMTP_PORT'), // Port for TLS (use 465 for SSL)
-                auth: {
-                    user: env('SMTP_USER'), // Your email address
-                    pass: env('SMTP_PASSWORD'), // Your email password or app-specific password
-                },
-                // Optional: Add additional options here if needed
-                secure: false, // Use true for 465, false for other ports
-            },
-            settings: {
-                defaultFrom: env('SMTP_USER'), // Default sender email
-                defaultReplyTo: env('SMTP_USER'), // Default reply-to email
-            },
-        },
+module.exports = {
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true,
+      },
     },
-    graphql: {
-        config: {
-            apolloServer: {
-                tracing: false,
-            },
-        },
-    },
-});
+  },
+};
