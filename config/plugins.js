9c95b3b93dad077586ccf766a30357bcd5e78a0a
warwickunirealale <warwickunirealale@gmail.com>
2025-01-22 10:10:26 +0000

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -21,7 +21,7 @@ module.exports = ({ env }) => ({
     graphql: {
         config: {
             apolloServer: {
-                tracing: false,
+                tracing: true,
             },
         },
     },
